6ada2b4f8e594f7aa603ad9d6c4ab649fb071a16
Ninetta11 <ninawelsh1@outlook.com>
2020-11-05 12:39:30 +1100

    Addition of logo, completion of AddEmployee, ViewAllEmployees functions

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
-
+const logo = require('asciiart-logo');
+const config = require('./package.json');
 
 // creates connection with database
 var connection = mysql.createConnection({
@@ -15,8 +16,7 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    start();
-});
+})
 
 // activates questions for user selection of action
 function start() {
@@ -27,8 +27,8 @@ function start() {
             name: "selection",
             choices: [
                 "View all Employees",
-                "View all Employees by Department",
-                "View all Employees by Manager",
+                "View Employees by Department",
+                "View Employees by Manager",
                 "Add Employee",
                 "Remove Employee",
                 "Update Employee Role",
@@ -44,10 +44,10 @@ function start() {
             case "View all Employees":
                 viewAllEmployees();
                 break;
-            case "View all Employees by Department":
+            case "View Employees by Department":
                 viewEmployeesByDepartment();
                 break;
-            case "View all Employees by Manager":
+            case "View Employees by Manager":
                 viewEmployeesByManager();
                 break;
             case "Add Employee":
@@ -77,26 +77,183 @@ function start() {
 
 // displays all employees 
 function viewAllEmployees() {
-    connection.query("SELECT * FROM employee", function (err, res) {
-        if (err) throw err;
-        console.log("All Employees");
-        console.table(["ID", "First Name", "Surname", "Role", "Manager"], res);
-    })
+    connection.query
+        ("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary, manager_id FROM employee, role, department WHERE employee.role_id = role.id AND role.department_id = department.id", function (err, res) {
+            if (err) throw err;
+            //matches manager id with employees and returns full name if manager exists
+            for (var i = 0; i < res.length; i++) {
+                if (res[i].manager_id === null) {
+                    res[i].manager = "No Manager";
+                }
+                else {
+                    let managerId = res[i].manager_id;
+                    let manager = res.find((employee) => employee.id === managerId);
+                    let managerName = manager.first_name + " " + manager.last_name;
+                    res[i].manager = managerName;
+                }
+                delete res[i].manager_id;
+            }
+            console.table("All Employees", res);
+            start();
+        })
 }
 
 // displays employees by department
 function viewEmployeesByDepartment() {
-    // required
-}
+    // returns all current departments from database
+    connection.query("SELECT department.name FROM department", function (err, res) {
+        if (err) throw err;
+        inquirer.prompt([
+            {
+                type: "list",
+                message: "Which Department would you like to view?",
+                name: "selection",
+                choices: res
+            }
+        ]).then(function (response) {
+            // displays employees from department based on user selection
+            connection.query(`SELECT employee.id, employee.first_name, employee.last_name, role.title FROM employee, role, department WHERE employee.role_id = role.id AND role.department_id = department.id AND department.name ="${response.selection}"`, function (err, res) {
+                if (err) throw err;
+                // checks for employees and returns response accordingly
+                if (res.length === 0) {
+                    console.log(`\n${response.selection} does not have any employees \n`);
+                }
+                else {
+                    console.log(`\n${response.selection}\n`);
+                    console.table(res);
+                }
+                start();
+            })
+        })
+    })
+};
 
 // displays employees by manager
 function viewEmployeesByManager() {
-
+    // displays all managers from database for user selection
+    connection.query(`SELECT employee.first_name, employee.last_name FROM employee WHERE employee.manager_id IS NULL`, function (err, res) {
+        let managers = [];
+        for (var i = 0; i < res.length; i++) {
+            managers.push(res[i].first_name + " " + res[i].last_name);
+        }
+        if (err) throw err;
+        // asks user for manager selection
+        inquirer.prompt([
+            {
+                type: "list",
+                message: "Which managers team would you like to view?",
+                name: "selection",
+                choices: managers
+            }
+        ]).then(function (response) {
+            // displays employees by manager based on user selection
+            let selectedManager = response.selection.split(" ");
+            connection.query(`SELECT employee.id FROM employee WHERE employee.first_name = "${selectedManager[0]}" AND employee.last_name = "${selectedManager[1]}"`, function (err, res) {
+                if (err) throw err;
+                connection.query(`SELECT employee.id, employee.first_name, employee.last_name, role.title FROM employee, role WHERE employee.manager_id = "${res[0].id}" AND employee.role_id = role.id`, function (err, res) {
+                    if (err) throw err;
+                    // checks for employees and returns response accordingly
+                    if (res.length === 0) {
+                        console.log(`\n${selectedManager[0]} ${selectedManager[1]} does not have any employees \n`);
+                    }
+                    else {
+                        console.log(`\n${response.selection}'s team\n`);
+                        console.table(res);
+                    }
+                    start();
+                })
+            })
+        })
+    })
 }
 
 // enables user to add a new employee
 function addEmployee() {
-    // required
+    let employees = ["No Manager"];
+    let roles = ["Add New Role"];
+    connection.query(`SELECT employee.first_name, employee.last_name FROM employee`, function (err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            employees.push(res[i].first_name + " " + res[i].last_name);
+        }
+    });
+    connection.query(`SELECT role.title FROM role`, function (err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            roles.push(res[i].title);
+        }
+    });
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is their first name?",
+            name: "firstname",
+            // The users input must be alphabetic
+            validate: val => /^[A-Za-z\s]+$/.test(val)
+        },
+        {
+            type: "input",
+            message: "What is their last name?",
+            name: "lastname",
+            // The users input must be alphabetic
+            validate: val => /^[A-Za-z\s]+$/.test(val)
+        },
+        {
+            type: "list",
+            message: "Select their Manager",
+            name: "manager",
+            choices: employees
+        },
+        {
+            type: "list",
+            message: "Select their Role",
+            name: "role",
+            choices: roles
+        }
+    ]).then(function (response) {
+        // if new role needs to be created for new employee, add role function activated
+        if (response.role === "Add New Role") {
+            console.log("Please add new role prior to adding new Employee");
+            addRole();
+        }
+        else {
+            // returning role id from database for role selected
+            connection.query(`SELECT role.id FROM role WHERE role.title = "${response.role}"`, function (err, res) {
+                if (err) throw err;
+                let roleId = res[0].id;
+                // if new employee has no manager, adding employee to database without managerId
+                if (response.manager === "No Manager") {
+                    addEmployeeToDatabase(response.firstname, response.lastname, roleId);
+                }
+                else {
+                    // returning manager id from database from user selection
+                    let selectedManager = response.manager.split(" ");
+                    connection.query(`SELECT employee.id FROM employee WHERE employee.first_name = "${selectedManager[0]}" AND employee.last_name = "${selectedManager[1]}"`, function (err, res) {
+                        if (err) throw err;
+                        let managerId = res[0].id;
+                        addEmployeeToDatabase(response.firstname, response.lastname, managerId, roleId);
+                    });
+                };
+            });
+        };
+    })
+}
+
+// adding new employee data to database
+function addEmployeeToDatabase(firstname, surname, roleId, managerId) {
+    connection.query(
+        "INSERT INTO employee SET ?",
+        {
+            first_name: firstname,
+            last_name: surname,
+            manager_id: managerId,
+            role_id: roleId
+        },
+        function (err, res) {
+            if (err) throw err;
+            console.log("New Staff Member added\n");
+            start();
+        })
 }
 
 // enables user to delete an employee 
@@ -107,6 +264,9 @@ function removeEmployee() {
 // enables user to update an employee role
 function updateEmployeeRole() {
     // required
+
+    // select all employees, and all roles
+    // inquirier prompt asking user to select employee, and then select which role they would like to update to with addNewROle as an option
 }
 
 // enables user to update an employee manager
@@ -116,7 +276,73 @@ function updateEmployeeManager() {
 
 // enables user to add a role
 function addRole() {
-    // required
+
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "role",
+            message: "Enter the title of the role you would like to add"
+        }
+    ]).then(function (response) {
+        let existingRoles = getRoles();
+        console.log(existingRoles);
+        let role = response.role;
+        if (existingRoles === role) {
+            console.log("This role already exists");
+        }
+        else {
+            let department = ["Add New Department"];
+            connection.query(`SELECT department.name FROM department`, function (err, res) {
+                if (err) throw err;
+                for (var i = 0; i < res.length; i++) {
+                    department.push(res[i].name);
+                }
+            });
+            inquirer.prompt([
+                {
+                    type: "input",
+                    name: "salary",
+                    message: "What is the annual salary of this role?",
+                    // The users input must be numeric
+                    validate: val => /^\d+$/.test(val)
+                },
+                {
+                    type: "list",
+                    message: "Select the Department for this role",
+                    name: "department",
+                    choices: department
+                }
+            ]).then(function (response) {
+                // department Id doesnt work
+                let departmentId = "";
+                connection.query(`SELECT id FROM department WHERE department.name = "${response.department}"`,
+                    function (err, res) {
+                        if (err) throw err;
+                        departmentId = res;
+                    });
+                connection.query(
+                    "INSERT INTO role SET ?",
+                    {
+                        title: role,
+                        salary: response.salary,
+                        department_id: departmentId
+                    },
+                    function (err, res) {
+                        if (err) throw err;
+                        console.log("New Role added:" + res.affectedRows + "\n");
+                        start();
+                    })
+            })
+        }
+    })
+}
+
+// returns list of current roles in database
+function getRoles() {
+    connection.query('SELECT role.title FROM role', function (err, res) {
+        if (err) throw err;
+        return res;
+    })
 }
 
 // enables user to add a department
@@ -127,3 +353,13 @@ function addDepartment() {
 function quit() {
 
 }
+
+
+function init() {
+    // renders app title
+    console.log(logo(config).render());
+    // activates user questions
+    start();
+}
+
+init();
